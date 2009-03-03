42bdb939fb40e5134cf7e1e39f9a7159be693bfa
palewire <ben.welsh@latimes.com>
2009-03-02 21:29:37 -0800

    Converted legacydb dumps from csv to pickles

--- legacydb/dump.py
+++ legacydb/dump.py
@@ -1,8 +1,11 @@
+# Should switch this to pickles
+
 from legacydb.legacy.models import *
 import codecs
 from django.utils.encoding import smart_str
 import os
 import csv
+import cPickle as pickle
 
 def get_csv_table(path):
 	app_dir = os.path.dirname(__file__)
@@ -12,42 +15,55 @@ def get_csv_table(path):
 def posts():
 	path = "dumps/posts.dump"
 	out = codecs.open(path, 'w', 'utf-8')
+	data = []
 	for post in WpPosts.objects.filter(post_status='publish', post_type='post'):
 		record = [post.id, post.post_date, post.post_title.strip(), post.post_content.strip()]
-		print >> out, "|".join([u'^%s^' % i for i in record])
+		data.append(record)
+	pickle.dump(record, out)
 	out.close()
 
 def comments():
 	path = "dumps/comments.dump"
 	out = codecs.open(path, 'w', 'utf-8')
+	data = []
 	for i in WpComments.objects.filter(comment_approved='1'):
 		record = [i.comment_post_id, i.comment_author, i.comment_author_email, i.comment_author_url, i.comment_author_ip, i.comment_date, i.comment_content]
-		print >> out, "|".join([u'^%s^' % i for i in record])
+		data.append([smart_str(i) for i in record])
+	#print data
+	pickle.dump(data, out)
 	out.close()
 
 def cats():
 	path = "dumps/categories.dump"
 	out = codecs.open(path, 'w', 'utf-8')
+	data = []
 	for i in WpTermTaxonomy.objects.filter(taxonomy='category'):
 		post_ids = WpTermRelationships.objects.filter(term_taxonomy_id=i.term_taxonomy_id)
 		name = WpTerms.objects.get(term_id=i.term_id).name
+		record = []
 		for post in post_ids:
-			print >> out, "|".join(map(str, [post.object_id, name]))
+			record.append([post.object_id, name])
+		data.append(record)
+	pickle.dump(data, out)
 	out.close()
 	
 def tags():
 	path = "dumps/tags.dump"
 	out = codecs.open(path, 'w', 'utf-8')
+	data = []
 	for i in WpTermTaxonomy.objects.filter(taxonomy='post_tag'):
 		post_ids = WpTermRelationships.objects.filter(term_taxonomy_id=i.term_taxonomy_id)
 		name = WpTerms.objects.get(term_id=i.term_id).name
+		record = []
 		for post in post_ids:
-			print >> out, "|".join(map(str, [post.object_id, name]))
+			record.append([post.object_id, name])
+		data.append([record])
+	pickle.dump(data, out)
 	out.close()
 	
-	#result = get_csv_table(path)
-	#for i, row in enumerate(result):
-	#	print row
-	
-	
+def run():
+	posts()
+	comments()
+	cats()
+	tags()
 
