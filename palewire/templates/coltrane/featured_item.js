aa87bb68a462060e8bbd08e262e41f6b557f6d64
palewire <ben.welsh@latimes.com>
2009-03-14 18:34:21 -0700

    Nested teaser in skybox block, so it only appears on the homepage

--- palewire/templates/coltrane/featured_item.js
+++ palewire/templates/coltrane/featured_item.js
@@ -1,8 +1,16 @@
-		
-	$(".btn-slide").click(function(){
-		$("#panel").slideToggle("slow");
+	
+	function slideBox () {
+		$("#panel").slideToggle(1000);
 		$(".btn-label-up").toggle();
 		$(".btn-label-down").toggle();
 		$(this).toggleClass("active"); 
 		return false;
+	}
+	
+	$(".btn-slide").click(function(){
+		slideBox();
 	});
+	
+	// Drop the box down and then up again as a tease.
+	slideBox();
+	slideBox();
