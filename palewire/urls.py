beae1627c724e4ce7a6e44ee3c2bd2159852bcd1
palewire <ben.welsh@latimes.com>
2009-03-15 22:44:01 -0700

    Tightened up XHTML on ticker_list; Tightened up redirects from old Wordpress structure in root urls.py

--- palewire/urls.py
+++ palewire/urls.py
@@ -6,7 +6,22 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+# Redirects from the old Wordpress URL structure to the new Django one.
+urlpatterns = patterns('django.views.generic.simple',
+	# Redirect links to old blog to new posts
+	(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'redirect_to', 
+		{'url': '/posts/%(year)s/%(month)s/%(day)s/%(slug)s/'}
+	),
+	# Redirects old tag pages to the new tag pages
+	(r'^tag/(?P<tag>[^/]+)/$', 'redirect_to', {'url': '/tags/%(tag)s/'}),
+	# Redirects old feeds to new feeds
+	(r'^comments/feed/$', 'redirect_to', {'url': '/feeds/comments/'}),
+	(r'^feed/$', 'redirect_to', {'url': '/feeds/posts/'}),
+	(r'^feed/atom/$', 'redirect_to', {'url': '/feeds/posts/'}),
+	(r'^feed/rss/$', 'redirect_to', {'url': '/feeds/posts/'}),
+)
+
+urlpatterns += patterns('',
 
 	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	(r'^admin/(.*)', admin.site.root),
@@ -16,12 +31,6 @@ urlpatterns = patterns('',
 	(r'^feeds/', include('coltrane.urls.feeds')),
 	(r'^links/', include('coltrane.urls.links')),
 	(r'^tags/', include('coltrane.urls.tags')),
-	(r'^tag/', include('coltrane.urls.tags')),
-	# Redirect links to old blog to new posts
-	(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 
-		'django.views.generic.simple.redirect_to', 
-		{'url': '/posts/%(year)s/%(month)s/%(day)s/%(slug)s/'}
-	),
 	(r'^posts/', include('coltrane.urls.posts')),
 	(r'^shouts/', include('coltrane.urls.shouts')),
 	(r'^videos/', include('coltrane.urls.videos')),
@@ -33,4 +42,3 @@ urlpatterns = patterns('',
 	(r'^ticker/', include('coltrane.urls.ticker')),
 	(r'^search/', include('solango.urls')),
 )
-
