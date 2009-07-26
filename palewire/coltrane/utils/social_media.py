b22606edd9bebbb3cd29696a959e94a1ab7c029e
palewire <ben.welsh@latimes.com>
2009-07-26 12:11:04 -0700

    Added social media links as a dictionary in case I want to call them in with a templatetag or something.

---
+++ palewire/coltrane/utils/social_media.py
@@ -0,0 +1,15 @@
+"""
+My constellation of social media sites that can be fetched and published.
+"""
+FREQUENTLY_USED = {
+	'Delicious': 'http://delicious.com/palewire',
+	'Digg': 'http://digg.com/users/palewire',
+	'Facebook': 'http://www.facebook.com/palewire',
+	'Flickr': 'http://www.flickr.com/photos/77114776@N00/',
+	'Github': 'http://github.com/palewire/',
+	'Kiva': 'http://www.kiva.org/lender/palewire',
+	'Last.FM': 'http://www.last.fm/user/palewire',
+	'LinkedIn': 'http://www.linkedin.com/pub/ben-welsh/9/3b4/256',
+	'Readernaut': 'http://readernaut.com/palewire/',
+	'Twitter': 'http://twitter.com/palewire',
+}
