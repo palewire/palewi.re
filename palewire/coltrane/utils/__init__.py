48d77ea85cfc9034e2999a3c8adb846c2d0c377a
palewire <ben.welsh@latimes.com>
2009-07-28 08:36:46 -0700

    Cleaned up API scripts a little bit

--- palewire/coltrane/utils/__init__.py
+++ palewire/coltrane/utils/__init__.py
@@ -1,96 +1,118 @@
+# http
 import httplib2
+
+# Date manipulation
 import dateutil.parser
 import dateutil.tz
+
+# Serialization
 from django.utils import simplejson
-from django.utils.encoding import force_unicode
 from anyetree import etree
+
+# Text manipulation
+from django.utils.encoding import force_unicode
+
+# Django
 from django.conf import settings
- 
+
 DEFAULT_HTTP_HEADERS = {
-    "User-Agent" : "Jellyroll/1.0 (http://code.google.com/p/jellyroll)"
+	"User-Agent" : "palewire.com"
 }
- 
+
 #
 # URL fetching sugar
 #
-    
+
 def getxml(url, **kwargs):
-    """Fetch and parse some XML. Returns an ElementTree"""
-    xml = fetch_resource(url, **kwargs)
-    return etree.fromstring(xml)
-    
+	"""
+	Fetch and parse some XML. Returns an ElementTree.
+	"""
+	xml = fetch_resource(url, **kwargs)
+	return etree.fromstring(xml)
+
+
 def getjson(url, **kwargs):
-    """Fetch and parse some JSON. Returns the deserialized JSON."""
-    json = fetch_resource(url, **kwargs)
-    return simplejson.loads(json)
- 
+	"""
+	Fetch and parse some JSON. Returns the deserialized JSON.
+	"""
+	json = fetch_resource(url, **kwargs)
+	return simplejson.loads(json)
+
+
 def fetch_resource(url, method="GET", body=None, username=None, password=None, headers=None):
-    h = httplib2.Http(timeout=15)
-    h.force_exception_to_status_code = True
-    
-    if username is not None or password is not None:
-        h.add_credentials(username, password)
-    
-    if headers is None:
-        headers = DEFAULT_HTTP_HEADERS.copy()
-    
-    response, content = h.request(url, method, body, headers)
-    return content
-    
+	"""
+	Fetch an URL. Return the content.
+	"""
+	h = httplib2.Http(timeout=15)
+	h.force_exception_to_status_code = True
+	
+	if username is not None or password is not None:
+		h.add_credentials(username, password)
+	
+	if headers is None:
+		headers = DEFAULT_HTTP_HEADERS.copy()
+	
+	response, content = h.request(url, method, body, headers)
+	return content
+	
 #
 # Date handling utils
 #
  
 def parsedate(s):
-    """
-Convert a string into a (local, naive) datetime object.
-"""
-    dt = dateutil.parser.parse(s)
-    if dt.tzinfo:
-        dt = dt.astimezone(dateutil.tz.tzlocal()).replace(tzinfo=None)
-    return dt
-            
+	"""
+	Convert a string into a local, naive datetime object.
+	"""
+	dt = dateutil.parser.parse(s)
+	if dt.tzinfo:
+		dt = dt.astimezone(dateutil.tz.tzlocal()).replace(tzinfo=None)
+	return dt
+
+
 def safeint(s):
-    """Always returns an int. Returns 0 on failure."""
-    try:
-        return int(force_unicode(s))
-    except (ValueError, TypeError):
-        return 0
+	"""
+	Always returns an int. Returns 0 on failure.
+	"""
+	try:
+		return int(force_unicode(s))
+	except (ValueError, TypeError):
+		return 0
+
 
 JELLYROLL_ADJUST_DATETIME = False
 if hasattr(settings,'JELLYROLL_ADJUST_DATETIME'):
-    JELLYROLL_ADJUST_DATETIME = settings.JELLYROLL_ADJUST_DATETIME
+	JELLYROLL_ADJUST_DATETIME = settings.JELLYROLL_ADJUST_DATETIME
 
 if JELLYROLL_ADJUST_DATETIME:
-    try:
-        import pytz
-    except ImportError:
-        import logging
-        log = logging.getLogger('jellyroll.providers.utils')
-        log.error("Cannot import pytz package and consequently, all datetime objects will be naive. "
-                  "In this particular case, e.g., all commit dates will be expressed in UTC.")
+	try:
+		import pytz
+	except ImportError:
+		import logging
+		log = logging.getLogger('jellyroll.providers.utils')
+		log.error("Cannot import pytz package and consequently, all datetime objects will be naive. "
+				  "In this particular case, e.g., all commit dates will be expressed in UTC.")
 
-    import datetime
-    import time
+	import datetime
+	import time
 
-    UTC = pytz.timezone('UTC')
-    LOCAL = pytz.timezone(settings.TIME_ZONE)
+	UTC = pytz.timezone('UTC')
+	LOCAL = pytz.timezone(settings.TIME_ZONE)
 
-    def utc_to_local_datetime(dt):
-        """
-        Map datetime as UTC object to it's localtime counterpart.
-        """
-        return dt.astimezone(LOCAL)
+	def utc_to_local_datetime(dt):
+		"""
+		Map datetime as UTC object to it's localtime counterpart.
+		"""
+		return dt.astimezone(LOCAL)
 
-    def utc_to_local_timestamp(ts, orig_tz=UTC):
-        """
-        Convert a timestamp object into a tz-aware datetime object.
-        """
-        timestamp = datetime.datetime.fromtimestamp(ts,tz=orig_tz)
-        return timestamp.astimezone(LOCAL)
+	def utc_to_local_timestamp(ts, orig_tz=UTC):
+		"""
+		Convert a timestamp object into a tz-aware datetime object.
+		"""
+		timestamp = datetime.datetime.fromtimestamp(ts,tz=orig_tz)
+		return timestamp.astimezone(LOCAL)
 
-    def utc_to_local_timestruct(ts, orig_tz=UTC):
-        """
-        Convert a timestruct object into a tz-aware datetime object.
-        """
-        return utc_to_local_timestamp(time.mktime(ts),orig_tz)
+	def utc_to_local_timestruct(ts, orig_tz=UTC):
+		"""
+		Convert a timestruct object into a tz-aware datetime object.
+		"""
+		return utc_to_local_timestamp(time.mktime(ts),orig_tz)
--- palewire/coltrane/utils/delicious.py
+++ palewire/coltrane/utils/delicious.py
@@ -1,10 +1,5 @@
 import os
 import sys
-import time
-import dateutil.parser
-import dateutil.tz
-import logging
-import urllib
 
 # Set the directories and django config so it can be run from cron.
 current_dir = os.path.abspath(__file__)
@@ -13,18 +8,28 @@ os.environ['PYTHONPATH'] = projects_dir
 sys.path.append(projects_dir)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
+# Django
 from django.conf import settings
+
+# Models
+from coltrane.models import Link
+
+# Date manipulation
+import time
+import dateutil.parser
+import dateutil.tz
+
+# Helpers
+import urllib
+import logging
+from coltrane import utils
 from django.db import transaction
 from django.utils.encoding import smart_unicode
-from coltrane.models import Link
-from coltrane import utils
 
-#
-# Super-mini Delicious API
-#
+
 class DeliciousClient(object):
 	"""
-	A super-minimal delicious client :)
+	A super-minimal delicious client.
 	"""
 	
 	lastcall = 0
@@ -49,51 +54,62 @@ class DeliciousClient(object):
 		xml = utils.getxml(url, username=self.username, password=self.password)
 		return xml
 
-#
-# Public API
-#
 
 log = logging.getLogger("coltrane.utils.delicious")
 
+
 def enabled():
+	"""
+	Test whether the proper settings have been configured by the user.
+	"""
 	ok = hasattr(settings, 'DELICIOUS_USER') and hasattr(settings, 'DELICIOUS_PASSWORD')
 	if not ok:
 		log.warn('The Delicious provider is not available because the '
 				 'DELICIOUS_USERNAME and/or DELICIOUS_PASSWORD settings are '
 				 'undefined.')
 	return ok
-	
+
 def update():
+	"""
+	Updates Delicious data and syncs it to the Link model.
+	"""
 	delicious = DeliciousClient(settings.DELICIOUS_USER, settings.DELICIOUS_PASSWORD)
 
-	# Check to see if we need an update
+	# Check to see if we need an update.
 	last_update_date = Link.sync.get_last_update()
-	last_post_date = utils.parsedate(delicious.posts.update().get("time"))
-	if last_post_date <= last_update_date:
-		log.info("Skipping update: last update date: %s; last post date: %s", last_update_date, last_post_date)
-		return
-	
-
 	for datenode in reversed(list(delicious.posts.dates().getiterator('date'))):
 		dt = utils.parsedate(datenode.get("date"))
+		# If the date in the record is the same or newer than the date of the last update..
 		if dt.date() >= last_update_date.date():
+			# ... pass the data along.
 			_update_bookmarks_from_date(delicious, dt)
 				
-#
-# Private API
-#
 
 def _update_bookmarks_from_date(delicious, dt):
+	"""
+	Retrieves and processes all posts from the submitted date.
+	"""
 	log.debug("Reading bookmarks from %s", dt)
+	
+	# Grab all the posts
 	xml = delicious.posts.get(dt=dt.strftime("%Y-%m-%d"))
+	
+	# Loop through them
 	for post in xml.getiterator('post'):
+		# Pull out all the attributes into a dictionary.
 		info = dict((k, smart_unicode(post.get(k))) for k in post.keys())
 		log.debug("Handling bookmark of %r", info["href"])
-		print info
+		
+		# Pass the dictionary along.
 		_handle_bookmark(info)
+
 _update_bookmarks_from_date = transaction.commit_on_success(_update_bookmarks_from_date)
 
+
 def _handle_bookmark(info):
+	"""
+	Accept a data dictionary drawn from the Delicious API and syncs it to the database.
+	"""
 	try:
 		# Just test the URL in case it's already been logged by another bookmarking service like Delicious.
 		l = Link.objects.get(url=info['href'])
