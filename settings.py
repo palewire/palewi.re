22f3c0349fee93cb07068b1977238d7d5ce5d1b5
Ben <ben@loftbox.(none)>
2010-01-22 07:56:52 -0800

    Added memcached settings.

--- settings.py
+++ settings.py
@@ -18,6 +18,11 @@ MEDIA_ROOT = os.path.join(settings_dir, 'media')
 MEDIA_URL = '/media/'
 ADMIN_MEDIA_PREFIX = '/media/admin/'
 
+CACHE_BACKEND =	'memcached://127.0.0.1:11211'
+CACHE_MIDDLEWARE_SECONDS = 60 *	5
+CACHE_MIDDLEWARE_KEY_PREFIX = 'palewire'                  
+CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
