d68c16578a6b8a2221119fc37cd02b102c40c454
palewire <ben.welsh@gmail.com>
2010-12-19 20:02:03 -0800

    Revised the github updater so it's one consolidated class instead of all the scattered stuff

--- coltrane/utils/github.py
+++ coltrane/utils/github.py
@@ -23,116 +23,91 @@ from coltrane.models import Commit
 
 # Logging
 from qiklog import QikLog
-logger = QikLog("jellyroll.utils.github")
-
-def enabled():
-    ok = hasattr(settings, 'GITHUB_USER')
-    if not ok:
-        log.warn('The Github provider is not available because the GITHUB_USER is undefined.')
-    return ok
 
 
 class GithubClient(object):
     """
     A minimal Github client. 
     """
+    GITHUB_TITLE_REGEX = re.compile(r'palewire pushed to (?P<branch>(.*)) at (?P<repository>(.*))')
+    logger = QikLog("jellyroll.utils.github")
     
     def __init__(self, username):
         self.username = username
-        
+        self.feed_url = 'http://github.com/%s.atom' % self.username
+
     def __getattr__(self):
         return GithubClient(self.username)
         
     def __repr__(self):
         return "<GithubClient: %s>" % self.username
         
-    def __call__(self):
-        url = 'http://github.com/%s.atom' % self.username
-        xml = utils.getxml(url)
-        commits = []
-        
-        GITHUB_TITLE_REGEX = re.compile(r'palewire pushed to (?P<branch>(.*)) at (?P<repository>(.*))')
-        
-        # Loop through all the entries
-        entries = list(xml.getiterator('{http://www.w3.org/2005/Atom}entry'))
-        for entry in entries:
-            
-            # Grab the date
-            pub_date = entry.find('{http://www.w3.org/2005/Atom}published').text
-            
-            # Grab the title
+    def get_latest_data(self):
+        self.xml = utils.getxml(self.feed_url)
+        self.commit_list = []
+        self.entry_list = self.xml.getiterator('{http://www.w3.org/2005/Atom}entry')
+        for entry in self.entry_list:
             title = entry.find('{http://www.w3.org/2005/Atom}title').text
-            
-            # Test it against our regex
-            match = GITHUB_TITLE_REGEX.search(title)
-            
+            match = self.GITHUB_TITLE_REGEX.search(title)
             # If it doesn't match, it's one of the less important entries
             # like when you start watching somebody's repo.
             if not match:
                 # And we can just skip those
                 continue
-                
-            # Grab the HTML with the commits
+            pub_date = self._extract_entry_pubdate(entry)
             html = entry.find('{http://www.w3.org/2005/Atom}content').text
             soup = BeautifulSoup(html)
             commits_html = soup.find('div', attrs={'class': 'commits'}).findAll('li')
-
-            # Loop through the one-to-many commits
             for commit_html in commits_html:
-                
                 # Create a dict to stuff the goodies
-                entry_dict = {}
-                entry_dict['pub_date'] = dateutil.parser.parse(pub_date).strftime('%Y-%m-%d %H:%M:%S')
-                
-                # Add the matches to our dictionary
-                entry_dict['branch'] = smart_unicode(match.group('branch'))
-                entry_dict['repository'] = smart_unicode(match.group('repository'))
-                
-                # Add the others
-                entry_dict['url'] = u'http://github.com%s' % smart_unicode([i['href'] for i in commit_html.findAll('a') if re.search('commit', i['href'])][0])
-                
-                entry_dict['message'] = smart_unicode(commit_html.find('blockquote').string.strip())
-                
+                entry_dict = {
+                    'pub_date': pub_date,
+                    'branch': smart_unicode(match.group('branch')),
+                    'repository': smart_unicode(match.group('repository')),
+                    'message': self._extract_commit_message(commit_html),
+                    'url': self._extract_commit_url(commit_html),
+                }
                 # Add the dict to the entry list
-                commits.append(entry_dict)
-                
-        return commits
-
-
-def update():
-    """
-    When executed, will update the database with the latest Gibhub commits.
-    """
-    # Init the GithubClient
-    github = GithubClient(settings.GITHUB_USER)
-
-    # Fetch the data
-    github_data = github()
+                self.commit_list.append(entry_dict)
+        # Pass out the commit_list
+        return self.commit_list
     
-    # Process the data
-    [_handle_commit(i) for i in github_data]
+    def _extract_entry_pubdate(self, entry):
+        text = entry.find('{http://www.w3.org/2005/Atom}published').text
+        dt = dateutil.parser.parse(text)
+        return dt.strftime('%Y-%m-%d %H:%M:%S')
+    
+    def _extract_commit_url(self, commit_html):
+        href_list = [i['href'] for i in commit_html.findAll('a')]
+        commit_url = [i for i in href_list if re.search('commit', i)][0]
+        return u'http://github.com%s' % smart_unicode(commit_url)
+    
+    def _extract_commit_message(self, commit_html):
+        s = commit_html.find('blockquote').string.strip()
+        return smart_unicode(s)
+    
+    def update(self):
+        [self._handle_commit(i) for i in self.get_latest_data()]
+    
+    def _handle_commit(self, commit_dict):
+        try:
+            c = Commit.objects.get(url=commit_dict['url'])
+            self.logger.log.debug("Commit %s already exists." % c)
+        except Commit.DoesNotExist:
+            c = Commit(
+                url = commit_dict['url'],
+                pub_date = commit_dict['pub_date'],
+                repository = commit_dict['repository'],
+                branch = commit_dict['branch'],
+                message = commit_dict['message'],
+                )
+            c.save()
+            self.logger.log.debug("Adding commit %s." % c)
 
 
-def _handle_commit(commit_dict):
-    logger.log.debug("Handling commit from %s", commit_dict['repository'])
-    try:
-        c = Commit.objects.get(url=commit_dict['url'])
-        logger.log.debug("Commit %s already exists." % c)
-    except Commit.DoesNotExist:
-        c = Commit(
-            url = commit_dict['url'],
-            pub_date = commit_dict['pub_date'],
-            repository = commit_dict['repository'],
-            branch = commit_dict['branch'],
-            message = commit_dict['message'],
-            )
-        c.save()
-        logger.log.debug("Adding commit %s." % c)
-        
-        
 if __name__ == '__main__':
-    update()
+    github = GithubClient(settings.GITHUB_USER)
+    github.update()
         
         
-        
-    
+
