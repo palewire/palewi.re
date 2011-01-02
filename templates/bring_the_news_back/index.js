b3ac3dc4c18770d71ead4aaf14da163fbd7f3414
palewire <ben.welsh@gmail.com>
2011-01-02 07:34:58 -0800

    Nuked the timeout feature from the jquery sound plugin to correct for weird timeout bug I'm having.

--- templates/bring_the_news_back/index.js
+++ templates/bring_the_news_back/index.js
@@ -1,16 +1,22 @@
 {% load media_url %}
-    $(document).ready(function(){
-        $(function(){
-            $.fn.supersized.options = {  
-                startwidth: 1024,  
-                startheight: 683,
-                vertical_center: 1,
-                slides : [
-                    {image : '{% media_url "img/krs-1024x683.jpg" %}' }
-                ]
-            };
-            $('#supersized').supersized();
-            var soundfile = "http://palewire.s3.amazonaws.com/bring_the_news_back/clip.mp3"
-            $.sound.play(soundfile);
-        });
-    });
+
+var superSize = function() {
+    $.fn.supersized.options = {  
+        startwidth: 1024,  
+        startheight: 683,
+        vertical_center: 1,
+        slides : [
+            {image : '{% media_url "img/krs-1024x683.jpg" %}' }
+        ]
+    };
+    $('#supersized').supersized();
+}
+var dropScience = function() {
+    var soundfile = "http://palewire.s3.amazonaws.com/bring_the_news_back/clip.wav"
+    $.sound.play(soundfile);//, {'timeout': 0});
+    //$("#content").html("<p>Bring the news back!</p>");
+}
+$(document).ready(function(){
+    superSize();
+    dropScience();
+});
