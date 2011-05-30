fdbfe039d2d6e9bb04cd02ad1c7eebd3cb60cfbc
palewire <ben.welsh@gmail.com>
2011-05-30 13:42:16 -0700

    Removed tags and other cruft from sitemaps

--- coltrane/sitemaps.py
+++ coltrane/sitemaps.py
@@ -1,30 +1,17 @@
 # Sitemaps
-from django.contrib.sitemaps import FlatPageSitemap, GenericSitemap, Sitemap
+from django.contrib.sitemaps import GenericSitemap, Sitemap
 
 # Models
 from coltrane.models import *
-from tagging.models import Tag
 
 post_dict = {
     'queryset': Post.live.all(),
     'date_field': 'pub_date',
 }
 
-tag_dict = {'queryset': Tag.objects.all(),}
 category_dict = {'queryset': Category.live.all(),}
 
-class TagSitemap(Sitemap):
-    priority = 0.6
-
-    def items(self):
-        return Tag.objects.all()
-
-    def location(self, obj):
-        return u'/tags/%s' % obj
-
 sitemaps = {
-    'about': FlatPageSitemap,
     'posts': GenericSitemap(post_dict, priority=0.9),
     'categories': GenericSitemap(category_dict, priority=0.6),
-    'tags': TagSitemap
 }
