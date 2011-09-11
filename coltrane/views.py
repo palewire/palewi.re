78c95447306ee756f5ab7297053f0300b75d87d4
palewire <ben.welsh@gmail.com>
2011-09-11 15:55:16 -0700

    Started to move in responsive design framework 1140.css, removed media_url tags from base template.

--- coltrane/views.py
+++ coltrane/views.py
@@ -31,15 +31,19 @@ def post_detail(request, year, month, day, slug):
     """
     date_stamp = time.strptime(year+month+day, "%Y%m%d")
     pub_date = datetime.date(*date_stamp[:3])
-    post = get_object_or_404(Post,    pub_date__year=pub_date.year,
-                                    pub_date__month=pub_date.month,
-                                    pub_date__day=pub_date.day,
-                                    slug=slug)
+    post = get_object_or_404(Post,
+        pub_date__year=pub_date.year,
+        pub_date__month=pub_date.month,
+        pub_date__day=pub_date.day,
+        slug=slug
+    )
     related_posts = TaggedItem.objects.get_related(post, get_model('coltrane', 'post'))[:5]
-    return render_to_response('coltrane/post_detail.html',
-                                { 'object': post,
-                                  'related_posts': related_posts })
-                                
+    context = {
+        'object': post,
+        'related_posts': related_posts
+    }
+    return direct_to_template(request, 'coltrane/post_detail.html', context)
+
 
 def category_detail(request, slug):
     """
