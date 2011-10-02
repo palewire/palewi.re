2b1a783eac068898eb26f3aafeef364d84d9dfd7
palewire <ben.welsh@gmail.com>
2011-10-02 11:58:54 -0700

    Kennedy name generator now working in new design.

--- kennedy/views.py
+++ kennedy/views.py
@@ -1,39 +1,37 @@
-# Helpers
 import random
 from django.shortcuts import render_to_response, get_object_or_404
-
-# Models
+from django.views.generic.simple import direct_to_template
 from kennedy.models import *
 
 
 def get_random_name(gender):
-	if gender not in ['male', 'female']:
-		return None
-	
-	fn = FirstName.objects.filter(gender=gender).order_by('?')[0].name
-	nn = '"%s"' % NickName.objects.filter(gender=gender).order_by('?')[0].name
-	ln = LastName.objects.all().order_by('?')[0].name
-	if gender == 'male':
-		suffixes = ['Jr.', 'II', 'III']
-		try:
-			suffix = suffixes[random.randrange(0,7)]
-		except IndexError:
-			suffix = ''
-	else:
-		suffix = ''
-	return u' '.join([fn.strip(), nn.strip(), ln.strip(), suffix]).strip()
-	
+    if gender not in ['male', 'female']:
+        return None
+    
+    fn = FirstName.objects.filter(gender=gender).order_by('?')[0].name
+    nn = '"%s"' % NickName.objects.filter(gender=gender).order_by('?')[0].name
+    ln = LastName.objects.all().order_by('?')[0].name
+    if gender == 'male':
+        suffixes = ['Jr.', 'II', 'III']
+        try:
+            suffix = suffixes[random.randrange(0,7)]
+        except IndexError:
+            suffix = ''
+    else:
+        suffix = ''
+    return u' '.join([fn.strip(), nn.strip(), ln.strip(), suffix]).strip()
+    
 
 
 def index(request):
-	context = {}
-	if request.method == 'GET':
-		first_name = request.GET.get('first_name', None)
-		gender = request.GET.get('gender', None)
-		if first_name and gender:
-			context.update({'first_name': first_name, 'gender': gender,})
-			kennedy_name = get_random_name(gender)
-			if kennedy_name:
-				context.update({'kennedy_name': kennedy_name})
-			
-	return render_to_response('kennedy/index.html', context)
+    context = {}
+    if request.method == 'GET':
+        first_name = request.GET.get('first_name', None)
+        gender = request.GET.get('gender', None)
+        if first_name and gender:
+            context.update({'first_name': first_name, 'gender': gender,})
+            kennedy_name = get_random_name(gender)
+            if kennedy_name:
+                context.update({'kennedy_name': kennedy_name})
+            
+    return direct_to_template(request, 'kennedy/index.html', context)
