2489b2b0eb597c5d059005987dea1308a1b3373b
palewire <ben.welsh@gmail.com>
2012-08-15 11:16:59 -0700

    style

--- coltrane/signals.py
+++ coltrane/signals.py
@@ -61,7 +61,7 @@ def category_count(sender, instance, signal, *args, **kwargs):
     for cat in Category.objects.all():
         cat.post_count = cat.get_live_post_count()
         cat.save()
-        
+
 
 def on_comment_was_posted(sender, comment, request, *args, **kwargs):
     """
@@ -69,17 +69,17 @@ def on_comment_was_posted(sender, comment, request, *args, **kwargs):
     
     Lifted from this guy: http://sciyoshi.com/blog/2008/aug/27/using-akismet-djangos-new-comments-framework/
     """
-
+    
     from django.contrib.sites.models import Site
     from django.conf import settings
     from django.template import Context, loader
     from django.core.mail import send_mail
-
+    
     try:
         from akismet import Akismet
     except:
         return
-
+    
     # use TypePad's AntiSpam if the key is specified in settings.py
     if hasattr(settings, 'TYPEPAD_ANTISPAM_API_KEY'):
         ak = Akismet(
@@ -92,7 +92,7 @@ def on_comment_was_posted(sender, comment, request, *args, **kwargs):
             key=settings.AKISMET_API_KEY,
             blog_url='http://%s/' % Site.objects.get(pk=settings.SITE_ID).domain
         )
-
+    
     if ak.verify_key():
         data = {
             'user_ip': request.META.get('REMOTE_ADDR', '127.0.0.1'),
