0061d59285cce1abca3de8f25d39dc4761f5854d
palewire <ben.welsh@gmail.com>
2014-09-11 15:09:29 -0700

    Whitespace

--- wxwtf/questionheds/models.py
+++ wxwtf/questionheds/models.py
@@ -10,13 +10,13 @@ class Item(models.Model):
     description = models.TextField(blank=True)
     pub_date = models.DateTimeField()
     source = models.CharField(max_length=1000)
-    
+
     class Meta:
         ordering = ("-pub_date",)
         get_latest_by = 'pub_date'
-    
+
     def __unicode__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return self.link
