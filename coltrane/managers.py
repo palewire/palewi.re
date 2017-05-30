297788ca52e1a52e0976335332356445e27449d7
palewire <ben.welsh@gmail.com>
2017-05-29 22:41:59 -0700

    Cut crap managers

--- coltrane/managers.py
+++ coltrane/managers.py
@@ -21,53 +21,6 @@ class LiveCategoryManager(models.Manager):
         return super(LiveCategoryManager, self).get_queryset().filter(post_count__gt=0)
 
 
-class TopDomainUpdateManager(models.Manager):
-    """
-    Updates the TopDomain model with the latest totals
-    """
-    def ranking(self, count=50, min_count=3, strata=6):
-        from urlparse import urlparse
-        from coltrane.models import Link
-        from coltrane.utils.cloud import calculate_cloud
-
-        # Fetch all the domains
-        domains = [urlparse(i.url)[1] for i in Link.objects.all()]
-
-        # Create a dict to stuff the counts
-        domain_count = {}
-
-        # Loop through all the domains
-        for d in domains:
-            try:
-                # If it exists in the dict, bump it up one
-                domain_count[d]['count'] += 1
-            except KeyError:
-                # If it doesn't exist yet, add it to the dict
-                domain_count[d] = {'count': 1, 'font-size': None}
-
-        # Sort the results as a list of tuples, from top to bottom
-        domain_tuple = domain_count.items()
-        domain_tuple.sort(lambda x,y:cmp(x[1], y[1]), reverse=True)
-
-        # Slice the limit and convert it to a dictionary
-        domain_dict = dict(domain_tuple[:count])
-
-        # Pass it into the cloud
-        object_list = calculate_cloud(
-            domain_dict, steps=strata, min_count=min_count, qs=False
-        )
-
-        # Empty out the model and refill it with the new data
-        self.model.objects.all().delete()
-        for obj in object_list:
-            self.model.objects.create(
-                name=obj['tag'],
-                count=obj['count'],
-                stratum=obj['font_size']
-            )
-        return True
-
-
 
 class SyncManager(models.Manager):
     """
@@ -86,53 +39,6 @@ class SyncManager(models.Manager):
             return qs.order_by('-pub_date')[0].pub_date
         except IndexError:
             return datetime.datetime.fromtimestamp(0)
-
-
-class TopTagUpdateManager(models.Manager):
-    """
-    Updates the TopTag model with the latest totals
-    """
-
-    def ranking(self, count=100, strata=6):
-        """
-        Refills the TopTag model with the most-commonly applied tags, using
-        the provided count value as its cut-off.
-        """
-        from django.db import connection
-        from tagging.models import TaggedItem, Tag
-        from coltrane.utils.cloud import calculate_cloud
-
-        # Pull the cloud data, excluding tracks
-        sql = """
-        SELECT t.name, count(*)
-        FROM tagging_taggeditem as ti
-        INNER JOIN tagging_tag as t ON ti.tag_id = t.id
-        WHERE ti.content_type_id
-        NOT IN (SELECT id FROM django_content_type WHERE name = 'track')
-        GROUP BY t.name
-        ORDER BY 2 DESC
-        LIMIT %s;
-        """ % count
-        cursor = connection.cursor()
-        cursor.execute(sql)
-        tag_counts = {}
-        for row in cursor.fetchall():
-            tag_counts[row[0]] = {'count': row[1], 'font-size': None}
-
-        # Pass it into the cloud
-        object_list = calculate_cloud(tag_counts, steps=strata, qs=False)
-
-        # Empty out the model and refill it with the new data
-        self.model.objects.all().delete()
-        for obj in object_list:
-            tag = Tag.objects.get(name=obj['tag'])
-            self.model.objects.create(
-                tag=tag,
-                name=obj['tag'],
-                count=obj['count'],
-                stratum=obj['font_size']
-            )
-        return True
 
 
 class GFKManager(models.Manager):
