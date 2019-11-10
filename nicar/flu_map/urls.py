0d5897cfdc5bf3f5faee3a562e31dc646c591d6f
palewire <ben.welsh@gmail.com>
2019-11-10 15:40:20 -0800

    Even more cruft

--- nicar/flu_map/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from nicar.flu_map import views
-
-
-urlpatterns = [
-    url(r'^$', views.index),
-]
--- nicar/polls/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from nicar.polls import views
-
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^(?P<poll_id>\d+)/$', views.detail),
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote),
-]
