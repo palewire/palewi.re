4152c26f018bf7768fe7718360ad58daf07359ac
palewire <ben.welsh@gmail.com>
2019-11-10 18:20:57 -0800

    Updated bio

--- fabfile/app.py
+++ fabfile/app.py
@@ -1,5 +1,5 @@
 import os
-from fabric.api import env, cd, sudo, task, local, run, settings
+from fabric.api import env, cd, sudo, task, local, run, settings, put
 
 
 @task
@@ -37,6 +37,7 @@ def deploy():
     pull()
     with settings(warn_only=True):
         clean()
+    pushsettings()
     pipinstall()
     collectstatic()
     publishstatic()
@@ -83,15 +84,14 @@ def pushsettings():
     """
     Push private files that aren't store in Git repository to remote hosts.
     """
-    for file_name in ["settings_prod.py", "newrelic.ini"]:
-        put(
-            os.path.join(env.local_project_dir, 'project', file_name),
-            os.path.join('/tmp', file_name),
-        )
-        sudo("mv %s %s" % (
-            os.path.join('/tmp', file_name),
-            os.path.join(env.project_dir, 'project', file_name),
-        ))
+    put(
+        os.path.join("./", 'project', "settings_prod.py"),
+        os.path.join('/tmp', "settings_prod.py"),
+    )
+    sudo("mv %s %s" % (
+        os.path.join('/tmp', "settings_prod.py"),
+        os.path.join(env.project_dir, 'project', "settings_prod.py"),
+    ))
 
 
 @task
