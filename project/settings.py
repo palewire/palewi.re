0f02266f806362a4144b81a03bec0bbfcfd14253
palewire <b@palewi.re>
2021-02-23 22:46:23 -0800

    Tricks

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_ROOT = os.path.join(BASE_DIR, ".staticfiles")
 SASS_PROCESSOR_ROOT = STATIC_ROOT
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "toolbox.storage.WhiteNoiseStaticFilesStorage"
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
