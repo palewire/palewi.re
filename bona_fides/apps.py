e5c9b63167e2bded65c8566c3dfe4e291f70653a
palewire <b@palewi.re>
2021-04-19 07:43:21 -0700

    Upgraded to Django 3.2

---
+++ bona_fides/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BonaFidesConfig(AppConfig):
+    name = 'bona_fides'
+    default_auto_field = 'django.db.models.BigAutoField'
---
+++ coltrane/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ColtraneConfig(AppConfig):
+    name = 'coltrane'
+    default_auto_field = 'django.db.models.BigAutoField'
