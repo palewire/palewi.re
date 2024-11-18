18289da27fc9fb264e469215a6fbe79fdb976b9b
Ben Welsh <b@palewi.re>
2024-11-18 06:13:13 -0500

    Update models.py

--- bona_fides/models.py
+++ bona_fides/models.py
@@ -52,7 +52,7 @@ class SocialMediaProfile(models.Model):
     url = models.CharField(max_length=1000)
 
     class Meta:
-        pass
+        ordering = ("title",)
 
     def __str__(self):
         return self.title
